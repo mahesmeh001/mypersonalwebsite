1cb8ed637a0d36b592ba889111ebc43b5d143067
mehul <mmaheshwari2@wisc.edu>
2022-08-04 14:22:23 -0500

    more content to music page

--- assets/js/elementslideshow.js
+++ assets/js/elementslideshow.js
@@ -47,6 +47,30 @@ function showSlides(n,e) {
             slides = document.getElementsByClassName("mySlides8");
             dots = document.getElementsByClassName("dot8");
             break;
+        case 9:
+            slides = document.getElementsByClassName("mySlides9");
+            dots = document.getElementsByClassName("dot9");
+            break;
+        case 10:
+            slides = document.getElementsByClassName("mySlides10");
+            dots = document.getElementsByClassName("dot10");
+            break;
+        case 11:
+            slides = document.getElementsByClassName("mySlides11");
+            dots = document.getElementsByClassName("dot11");
+            break;
+        case 12:
+            slides = document.getElementsByClassName("mySlides12");
+            dots = document.getElementsByClassName("dot12");
+            break;
+        case 13:
+            slides = document.getElementsByClassName("mySlides13");
+            dots = document.getElementsByClassName("dot13");
+            break;
+        case 14:
+            slides = document.getElementsByClassName("mySlides14");
+            dots = document.getElementsByClassName("dot14");
+            break;
     }
 
 
